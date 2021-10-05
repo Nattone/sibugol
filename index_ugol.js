7f550a450f7d2e2a399e0686c85558be5be2f2b1
Nata Malinova <rawr@raery.ru>
2021-10-06 04:05:19 +0700

    change example

--- index_ugol.js
+++ index_ugol.js
@@ -1,41 +1,104 @@
-const loader = new THREE.GLTFLoader();
+let stats, mixer, camera, scene, renderer, clock;
 
-// Load a glTF resource
-loader.load(
-    // resource URL
-    './models/SimpleSkinning.gltf',
-    // called when the resource is loaded
-    function (gltf) {
-        console.log('loaded');
+init();
+animate();
+
+function init() {
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(18, 6, 18);
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xa0a0a0);
+    scene.fog = new THREE.Fog(0xa0a0a0, 70, 100);
+
+    clock = new THREE.Clock();
+
+    // ground
+
+    const geometry = new THREE.PlaneGeometry(500, 500);
+    const material = new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false });
+
+    const ground = new THREE.Mesh(geometry, material);
+    ground.position.set(0, - 5, 0);
+    ground.rotation.x = - Math.PI / 2;
+    ground.receiveShadow = true;
+    scene.add(ground);
+
+    const grid = new THREE.GridHelper(500, 100, 0x000000, 0x000000);
+    grid.position.y = - 5;
+    grid.material.opacity = 0.2;
+    grid.material.transparent = true;
+    scene.add(grid);
+
+    // lights
+
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 0.6);
+    hemiLight.position.set(0, 200, 0);
+    scene.add(hemiLight);
+
+    const dirLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    dirLight.position.set(0, 20, 10);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 18;
+    dirLight.shadow.camera.bottom = - 10;
+    dirLight.shadow.camera.left = - 12;
+    dirLight.shadow.camera.right = 12;
+    scene.add(dirLight);
+
+    //
+
+    const loader = new GLTFLoader();
+    loader.load('./models/SimpleSkinning.gltf', function (gltf) {
 
         scene.add(gltf.scene);
 
-        gltf.animations; // Array<THREE.AnimationClip>
-        gltf.scene; // THREE.Group
-        gltf.scenes; // Array<THREE.Group>
-        gltf.cameras; // Array<THREE.Camera>
-        gltf.asset; // Object
+        gltf.scene.traverse(function (child) {
 
-    },
+            if (child.isSkinnedMesh) child.castShadow = true;
 
-    // called while loading is progressing
-    function (xhr) {
-        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    },
-    // called when loading has errors
-    function (error) {
-        console.log('An error happened');
-    }
-);
+        });
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight); // 70 - насколько широко видит камера
-const renderer = new THREE.WebGLRenderer();
+        mixer = new THREE.AnimationMixer(gltf.scene);
+        mixer.clipAction(gltf.animations[0]).play();
 
-scene.background = new THREE.Color(0x000fff); // фон для сцены
-renderer.setSize(window.innerWidth, window.innerHeight); //установка размера = текущ.ширина и текущ.высота
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.render(scene, camera);
-document.body.appendChild(renderer.domElement); // в эл-т body вставляем рендер
-camera.position.z = 5;
+    });
+
+    //
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    container.appendChild(renderer.domElement);
+
+    //
+
+    stats = new Stats();
+    container.appendChild(stats.dom);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enablePan = false;
+    controls.minDistance = 5;
+    controls.maxDistance = 50;
+
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+
+    if (mixer) mixer.update(clock.getDelta());
+
+    render();
+    stats.update();
+
+}
+
+function render() {
+
+    renderer.render(scene, camera);
+
+}
