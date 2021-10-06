ee3c719be95507f3ae1317221f512f68ce72a0cd
Nata Malinova <rawr@raery.ru>
2021-10-06 08:13:37 +0700

    Update index_ugol.js

--- index_ugol.js
+++ index_ugol.js
@@ -31,6 +31,7 @@ function init() {
 
     // const light = new THREE.AmbientLight(0x404040); // soft white light
     // scene.add(light);
+    // 0x404040 - soft white light
     light = new THREE.PointLight(0xffffff, 10, 100);
     light.position.set(-0.1, -0.1, 0.2);
     scene.add(light);
