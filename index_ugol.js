a347a9b433714b0842ad60ecb875b55a74840ab4
Nata Malinova <rawr@raery.ru>
2021-10-06 07:34:23 +0700

    Update index_ugol.js

--- index_ugol.js
+++ index_ugol.js
@@ -7,7 +7,7 @@ function init() {
     const container = document.getElementById('ugol');
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 20);
-    camera.position.set(0, 0, 0);
+    camera.position.set(0, 0, 0.2);
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xffffff);
