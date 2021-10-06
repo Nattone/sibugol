c7aa0088a77d4c72aaacf6544d39deaaebd565a3
Nata Malinova <rawr@raery.ru>
2021-10-06 07:50:53 +0700

    Update index_ugol.js

--- index_ugol.js
+++ index_ugol.js
@@ -51,8 +51,8 @@ function onWindowResize() {
 
 function animate() {
     requestAnimationFrame(animate);
-    object.rotation.x += 0.01;
-    object.rotation.y += 0.01;
+    camera.rotation.x += 0.01;
+    camera.rotation.y += 0.01;
     render()
 }
 
